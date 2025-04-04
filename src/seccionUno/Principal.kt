ef36d9120e79fa6b823298a752588c09b2d1552a
IvanGMtz <navi.garces24@gmail.com>
2025-04-04 11:18:25 -0500

    Data types

--- src/seccionUno/Principal.kt
+++ src/seccionUno/Principal.kt
@@ -1,5 +1,46 @@
 package seccionUno
 
 fun  main(){
-    println("Hola mundo 2")
+    /**
+     * TIPOS DE DATOS
+     * Primitivos Enteros:
+     * Long
+     * Int
+     * Short
+     * Byte
+     */
+    var numero = 1_000_000;
+
+    println(numero);
+
+    numero = 20;
+
+    println(numero)
+
+    /**
+     * TIPOS DE DATOS
+     * Primitivos Punto Flotante:
+     * double
+     * float
+     */
+
+    var pi = 3.14159265359;
+
+    println(pi)
+    var pi2 = 3.14159265359f;
+    println(pi2)
+
+    /**
+     * TIPOS DE DATOS
+     * Caracteres y Cadena de caracteres:
+     * Char
+     * String
+     */
+
+    val mensaje = 'c';
+
+    println(mensaje);
+
+    val msj = "String con comillas dobles y char 'simples' "
+    println(msj)
 }
